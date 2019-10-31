0ee5dca99198e3b65d7f945a1392558568c685ed
Reece Pritchard <rapritchard96@gmail.com>
2019-10-31 20:45:07 +0000

    Complex state and debugging

--- part1/complexstateanddebug/src/index.js
+++ part1/complexstateanddebug/src/index.js
@@ -1,12 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+
+const History = ({ allClicks }) => {
+  if(allClicks.length === 0) {
+    return (
+      <div>
+        The app is used by pressing the button
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      Button press history: {allClicks.join(' ')}
+    </div>
+  )
+}
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
+const App = (props) => {
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'));  
+    setLeft(left + 1);
+  };
+
+  const handleRightClick = () => {    
+    setAll(allClicks.concat('R'));    
+    setRight(right + 1)  ;
+  };
+
+  return (
+    <div>
+      <div>
+        {left}
+        <Button onClick={handleLeftClick} text={'Left'}/>
+        <Button onClick={handleRightClick} text={'Right'}/>
+        {right}
+        <History allClicks={allClicks} />
+      </div>
+    </div>
+  )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
