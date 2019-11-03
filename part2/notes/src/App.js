cdff257da5e66f975c89dd0e092c1fd8c147ac06
Reece Pritchard <rapritchard96@gmail.com>
2019-11-03 13:25:24 +0000

    Getting data from server for notes app

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,10 +1,23 @@
-import React, { useState } from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import Note from './components/Note';
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes);
+const App = () => {
+  const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState('');
   const [showAll, setShowAll] = useState(true);
+
+  const hook = () => {
+    console.log('effect');
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled');
+        setNotes(response.data);
+      })
+  };
+
+  useEffect(hook, []);
 
   const notesToShow = showAll ? notes : notes.filter(note => note.important);
 
