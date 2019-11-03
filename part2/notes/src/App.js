43bd8e8d315a619bb42e7b2bc35c77d6e3c25da1
Reece Pritchard <rapritchard96@gmail.com>
2019-11-03 17:14:41 +0000

    Altering data in a server

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,32 +1,33 @@
-import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import Note from './components/Note';
+import noteService from './services/notes';
 
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState('');
   const [showAll, setShowAll] = useState(true);
 
-  const hook = () => {
-    console.log('effect');
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        console.log('promise fulfilled');
-        setNotes(response.data);
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes);
       })
-  };
+  }, []);
 
-  useEffect(hook, []);
+  const toggleImportanceOf = id => {
+    const note = notes.find(n => n.id === id);
+    const changedNote = { ...note, important: !note.important };
 
-  const notesToShow = showAll ? notes : notes.filter(note => note.important);
-
-  const rows = () => notesToShow.map(note => 
-    <Note
-      key={note.id}
-      note={note}
-    />
-  );
+    noteService
+      .update(id, changedNote).then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote));
+      })
+      .catch(error => {
+        alert(`The note ${note.content} was already deleted from the server`);
+        setNotes(notes.filter(n => n.id !== id));
+      });
+  }
 
   const addNote = (event) => {
     event.preventDefault();
@@ -37,14 +38,27 @@ const App = () => {
       id: notes.length + 1,
     }
 
-    setNotes(notes.concat(noteObject));
-    setNewNote('');
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+          setNotes(notes.concat(returnedNote));
+          setNewNote('');
+      });
   }
 
+  const rows = () => notesToShow.map(note => 
+    <Note
+      key={note.id}
+      note={note}
+      toggleImportance={() => toggleImportanceOf(note.id)}
+    />
+  );
+
   const handleNoteChange = (event) => {
-    console.log(event.target.value);
     setNewNote(event.target.value);
   }
+
+  const notesToShow = showAll ? notes : notes.filter(note => note.important);
 
   return (
     <div>
